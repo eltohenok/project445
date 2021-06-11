fac745eadb73dca66036087a33f7b167486473fb
cs445 <79168610+eltohenok@users.noreply.github.com>
2021-06-10 17:05:11 -0700

    project.js

--- project.js
+++ project.js
@@ -1,28 +1,18 @@
-async function fetchSingleUser() {
-    let responseBody = await fetch('https://randomuser.me/api/');
-    let json = await responseBody.json();
-    return json.results[0];
-}
-
-async function fetchUsers(len) {
-    const users = [];
-    for (let i = 0; i < len; i++) {
-        users[i] = await fetchSingleUser();
+function log() {
+    let AccountHolder = {
+      userName: "elto",
+      password: "23456",
+    };
+    const userOne = document.getElementById("userOne-id").value;
+    const userOnepassword = document.getElementById("userOne-id").value;
+    if (
+      AccountHolder.userName === userOne &&
+      AccountHolder.password === userOnepassword
+    ) {
+      document.getElementById("user-class-id").tagName = "user-class-display";
+      document.getElementById("login-page-id").tagName = "displaylogin-page";
+      userActivity();
+    } else {
+      alert("wrong password");
     }
-    return users;
-}
-
-async function displayUserInHtml() {
-    const userArr = await fetchUsers(3);
-    for (let i = 0; i < userArr.length; i++) {
-        let user = userArr[i];
-        document.getElementById('img' + i).src = user.picture.large;
-        document.getElementById('name' + i).innerHTML = user.name.first + ' ' + user.name.last;
-        document.getElementById('phone' + i).innerHTML = 'phone:' + user.phone;
-        document.getElementById('email' + i).innerHTML = user.email;
-    }
-
-}
-
-
-
+  }
